0171bc7a1638e0f5cb2159ffabcaf3a55d124cc3
Łukasz Zaroda <luken@luken-tech.pl>
2023-07-05 15:29:11 +0200

    Configured CI tests.

---
+++ tests/bin/core-tests.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+DRAKY_SOURCE_PATH=${DRAKY_SOURCE_PATH-"$CWD/../.."}
+
+"${DRAKY_SOURCE_PATH}/core/bin/dk-test"
---
+++ tests/bin/functional-tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+DRAKY_SOURCE_PATH=${DRAKY_SOURCE_PATH-"$CWD/../.."}
+DRAKY="${DRAKY_SOURCE_PATH}/dist/bin/draky"
+
+export DRAKY_SOURCE_PATH
+export DRAKY
+
+DRAKY_TEST_IMAGE_PATH=$(find "${DRAKY_SOURCE_PATH}" -name '*.image.tar')
+
+docker load --input "${DRAKY_TEST_IMAGE_PATH}"
+
+cd "$CWD/../functional" || exit 1
+bats .
