df1bf9402727790533ee484585ee82f1d9eb4f0e
Łukasz Zaroda <luken@luken-tech.pl>
2025-05-29 13:24:19 +0200

    Check if the docker cache path directory already exists before creating it.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 DRAKY_DOCKER_CACHE_PATH=/.docker
 
 # This directory needs to exist and be writable by the host, for draky to be able to build images.
-mkdir "${DRAKY_DOCKER_CACHE_PATH}"
-chown "${DRAKY_HOST_UID}:${DRAKY_HOST_GID}" "${DRAKY_DOCKER_CACHE_PATH}"
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir "${DRAKY_DOCKER_CACHE_PATH}"
+  chown "${DRAKY_HOST_UID}:${DRAKY_HOST_GID}" "${DRAKY_DOCKER_CACHE_PATH}"
+fi
 
 exec "$@"
